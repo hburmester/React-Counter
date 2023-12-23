640c33a7e3df9fe5b335a120efa5f57aebd0792c
hburmester <hans.burmester@utec.edu.pe>
2023-12-23 11:01:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Counter = () => {
+  // State for the counter value
+  const [count, setCount] = useState(0);
+
+  // State for the step value
+  const [step, setStep] = useState(1);
+
+  // Effect to update the document title when count changes
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  // Event handler for increment button
+  const handleIncrement = () => {
+    setCount(count + step);
+  };
+
+  // Event handler for decrement button
+  const handleDecrement = () => {
+    setCount(count - step);
+  };
+
+  // Event handler for step input change
+  const handleStepChange = (event) => {
+    const newStep = parseInt(event.target.value, 10);
+    setStep(isNaN(newStep) ? 1 : newStep);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {count}</h1>
+      <label>
+        Step:
+        <input type="number" value={step} onChange={handleStepChange} />
+      </label>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Counter;
